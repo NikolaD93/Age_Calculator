88c83790a20635e87be6ad50ccd1ee63ac3734cf
Nikola Dojcinovic <goonergalactico@gmail.com>
2023-04-24 15:07:39 +0200

    year and month validation finish

--- script/main.js
+++ script/main.js
@@ -11,6 +11,10 @@ const monthWrapper = document.querySelector(".month-wrapper");
 const yearWrapper = document.querySelector(".year-wrapper");
 
 const form = document.querySelector("form");
+const dayErrorMsg = document.querySelector(".day-error-message");
+const monthErrorMsg = document.querySelector(".month-error-message");
+const yearErrorMsg = document.querySelector(".year-error-message");
+const dateErrorMsg = document.querySelector(".date-error-message");
 
 const calculateAge = (e) => {
   e.preventDefault();
@@ -19,23 +23,58 @@ const calculateAge = (e) => {
   const month = monthInput.value;
   const year = yearInput.value;
 
-  const inputDate = new Date(year, month - 1, day);
-
-  const diffInMs = Date.now() - inputDate.getTime();
-
-  const ageDate = new Date(diffInMs);
-  const years = ageDate.getUTCFullYear() - 1970;
-  const months = ageDate.getUTCMonth();
-  const days = ageDate.getUTCDate() - 1;
-
   if (day && month && year) {
-    dayDisplay.textContent = days;
-    monthDisplay.textContent = months;
-    yearDisplay.textContent = years;
+    if (day < 1 || day > 31) {
+      dayWrapper.classList.add("error");
+      dayErrorMsg.textContent = "Must be a valid day";
+    } else {
+      dayWrapper.classList.remove("error");
+    }
+
+    if (month < 1 || month > 12) {
+      monthWrapper.classList.add("error");
+      monthErrorMsg.textContent = "Must be a valid month";
+    } else {
+      monthWrapper.classList.remove("error");
+    }
+
+    if (year > new Date().getFullYear() || year < 1900) {
+      yearWrapper.classList.add("error");
+      yearErrorMsg.textContent = "Must be a valid year in the past";
+    } else {
+      yearWrapper.classList.remove("error");
+    }
+
+    const age = getAge(day, month, year);
+    if (age) {
+      dayDisplay.textContent = age.days;
+      monthDisplay.textContent = age.months;
+      yearDisplay.textContent = age.years;
+    } else {
+      dateErrorMsg.textContent = "Must be a valid date";
+    }
   } else {
     dayWrapper.classList.toggle("error", !day);
     monthWrapper.classList.toggle("error", !month);
     yearWrapper.classList.toggle("error", !year);
+  }
+};
+
+const getAge = (day, month, year) => {
+  const inputDate = new Date(year, month - 1, day);
+  if (
+    inputDate.getDate() === parseInt(day) &&
+    inputDate.getMonth() === parseInt(month) - 1 &&
+    inputDate.getFullYear() === parseInt(year)
+  ) {
+    const diffInMs = Date.now() - inputDate.getTime();
+    const ageDate = new Date(diffInMs);
+    const years = ageDate.getUTCFullYear() - 1970;
+    const months = ageDate.getUTCMonth();
+    const days = ageDate.getUTCDate() - 1;
+    return { years, months, days };
+  } else {
+    return null;
   }
 };
 
